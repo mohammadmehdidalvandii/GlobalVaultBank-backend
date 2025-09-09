ed80c57a307d3fa180bcaf2daed8ce00b896b263
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-09-09 09:46:43 +0330

    created model employee and interface

---
+++ src/models/Employee.ts
@@ -0,0 +1,69 @@
+import { DataTypes, STRING } from "sequelize";
+import connectToDB from "../config/db";
+
+const employee = connectToDB.define('Employee',{
+    id:{
+        type:DataTypes.UUID,
+        defaultValue:DataTypes.UUIDV4,
+        primaryKey:true,
+    },
+    employee_code:{
+        type:DataTypes.STRING,
+        allowNull:false,
+        unique:true,
+    },
+    first_name:{
+        type:DataTypes.STRING,
+        allowNull:false,
+    },
+    last_name:{
+        type:DataTypes.STRING,
+        allowNull:false,
+    },
+    national_id:{
+        type:DataTypes.STRING,
+        allowNull:false,
+        unique:true,
+    },
+    email:{
+        type:DataTypes.STRING,
+        allowNull:false,
+        unique:true,
+        validate:{isEmail:true},
+    },
+    phone:{
+        type:DataTypes.STRING,
+        allowNull:false,
+    },
+    position:{
+        type:DataTypes.STRING,
+        allowNull:false,
+    },
+    department:{
+        type:DataTypes.STRING,
+        allowNull:false,
+    },
+    hire_data:{
+        type:DataTypes.DATE,
+        allowNull:false,
+    },
+    status:{
+        type:DataTypes.ENUM('active', 'inactive','suspended'),
+        defaultValue:'active',
+    },
+    created_at:{
+        type: DataTypes.DATE,
+        defaultValue:DataTypes.NOW,
+    },
+    updated_at:{
+        type:DataTypes.DATE,
+        defaultValue:DataTypes.NOW,
+    },
+},{
+    tableName:'employee',
+    timestamps:true,
+    createdAt:'created_at',
+    updatedAt:'updated_at'
+})
+
+export default employee
---
+++ src/types/employee.ts
@@ -0,0 +1,15 @@
+export interface IEmployee {
+    id: string;
+  employee_code: string;
+  first_name: string;
+  last_name: string;
+  national_id: string;
+  email: string;
+  phone: string;
+  position: string;
+  department: string;
+  hire_date: Date;
+  status: 'active' | 'inactive' | 'suspended';
+  created_at: Date;
+  updated_at: Date;
+}
