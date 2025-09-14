258c92bd1560487268705b4ef013c8f59f67e74b
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-09-14 10:34:10 +0330

    created model account

---
+++ src/models/Account.ts
@@ -0,0 +1,58 @@
+import { DataTypes } from "sequelize";
+import connectToDB from "../config/db";
+import customer from "./Customer";
+
+const account = connectToDB.define('account',{
+    id:{
+        type:DataTypes.UUID,
+        defaultValue:DataTypes.UUIDV4,
+        primaryKey: true,
+    },
+    customerId:{
+        type:DataTypes.UUID,
+        allowNull:false,
+        references:{
+            model:customer,
+            key:'id'
+        }
+    },
+    accountNumber:{
+        type:DataTypes.STRING(20),
+        allowNull: false,
+        unique: true
+    },
+    accountName:{
+        type:DataTypes.STRING(255),
+        allowNull:false,
+    },
+    type:{
+        type: DataTypes.ENUM('checking','savings','business','investment'),
+        allowNull: false,
+    },
+    currency:{
+        type:DataTypes.STRING(3),
+        defaultValue:'USD'
+    },
+    currencySymbol:{
+        type: DataTypes.STRING(5),
+        defaultValue:'$',
+    },
+    balance:{
+        type: DataTypes.DECIMAL(15,2),
+        defaultValue:0.00,
+    },
+    interestRate:{
+        type: DataTypes.DECIMAL(5,2),
+        defaultValue:0.00
+    },
+    status:{
+        type:DataTypes.ENUM('active','frozen','closed'),
+        defaultValue:'active'
+    },
+},{
+    timestamps:true,
+    underscored:true
+});
+
+
+export default account
