7d7bc73b34db711244eedff8d04152993a3d8bb8
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-09-15 09:44:12 +0330

    created logic accountValidation is fixed

--- src/routes/AccountRoutes.ts
+++ src/routes/AccountRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { accountController } from "../controllers/Account.controller";
+import { validationAccount } from "../validations/accountValidation";
 
 const router = Router();
 
-router.post('/create', accountController.create);
+router.post('/create',  validationAccount ,accountController.create);
 router.get('/', accountController.accountGetAll);
 router.get('/:id', accountController.accountGetById);
 router.delete('/delete/:id',accountController.delete);
---
+++ src/validations/accountValidation.ts
@@ -0,0 +1,19 @@
+import { body ,  validationResult } from "express-validator";
+import { Req , Res , Next } from "../types/express";
+
+export const  validationAccount = [
+    body('customerId').notEmpty().withMessage('customer ID is required').isUUID().withMessage('customer id is UUID'),
+    body('accountNumber').notEmpty().withMessage('accountNumber is required').isLength({min:10}).withMessage('accountNumber must be 10 number'),
+    body('type').isIn(['checking','savings','business','investment']).withMessage('account type is not valid'),
+    body('balance').optional().isFloat({min:0}).withMessage('balance just number '),
+    body('interestRate').optional().isFloat({min:0}).withMessage('interestRate just number'),
+    (req:Req ,res:Res , next:Next)=>{
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                error:errors.array()
+            })
+        }
+        next()
+    }
+]
