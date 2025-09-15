c8cedb2b14ea991f17b765c811c6b0a4e8819a61
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-09-15 11:27:57 +0330

    created association fixed

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,15 @@
 import CustomerModel from "./Customer";
 import AccountModel from "./Account";
+import TransactionModel from './Transaction';
 
+// Customer <-> Account
 CustomerModel.hasMany(AccountModel, {foreignKey:'customerId', as: 'accounts'});
 AccountModel.belongsTo(CustomerModel,{foreignKey:"customerId" , as: 'customer'});
+// Account <-> Transaction
+AccountModel.hasMany(TransactionModel , {foreignKey:'accountId' ,as:'transactions'});
+TransactionModel.belongsTo(AccountModel , {foreignKey:'accountId',as:'account'});
+// Customer <-> Transaction
+CustomerModel.hasMany(TransactionModel ,{foreignKey:'customerId',as:'transactions'});
+TransactionModel.belongsTo(CustomerModel, {foreignKey:'customerId', as:'customer'})
 
-export {CustomerModel , AccountModel}
+export {CustomerModel , AccountModel ,TransactionModel}
