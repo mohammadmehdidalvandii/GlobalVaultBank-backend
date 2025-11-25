5f098e21dc646c288c63a66518ca8cc4f6dae077
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-11-25 16:35:42 +0330

    fixed data backend

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,13 @@
 import {Sequelize}  from 'sequelize';
 
-const connectToDB = new Sequelize("globalbank","root","",{
-    host:'localhost',
+const connectToDB = new Sequelize("mary","root","ERln7VWKKU3CjU01",{
+    host:'services.irn2.chabokan.net',
+    port:58643,
     dialect:'mysql'
 })
+// const connectToDB = new Sequelize("globalbank","root","",{
+//     host:'localhost',
+//     dialect:'mysql'
+// })
 
 export default connectToDB
